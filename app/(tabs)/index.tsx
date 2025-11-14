a9f2f057495de6ab5b4fbbc94755f237de432661
kongyangsong <kongyangsong@baiwang.com>
2025-11-14 11:37:05 +0800

    feat: change ScrollView with FlatList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Habit } from "@/types";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import React, { useEffect, useRef } from "react";
-import { ScrollView, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
+import { FlatList, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import { Swipeable } from "react-native-gesture-handler";
 import { Surface, Text } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
@@ -48,80 +48,81 @@ const Index = () => {
     }
   };
 
+  const renderItem = (item: Habit) => (
+    <Swipeable
+      containerStyle={{ marginBottom: 18 }}
+      key={`${item.id}}`}
+      ref={(ref) => {
+        swipeableRefs.current[item.id] = ref;
+      }}
+      overshootLeft={false}
+      overshootRight={false}
+      renderRightActions={() => renderRightActions(item)}
+      onSwipeableOpen={(direction) => {
+        // if (direction === "left") {
+        // }
+        // if (direction === "right") {
+        // }
+
+        closeOtherSwiper(item.id);
+      }}
+    >
+      <Surface
+        elevation={0}
+        style={{
+          borderRadius: 18,
+          backgroundColor: item.light,
+          opacity: 0.8,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.08,
+          shadowRadius: 8,
+          elevation: 4,
+        }}
+      >
+        <TouchableWithoutFeedback onPress={() => closeOtherSwiper(item.id)}>
+          <View className="p-5">
+            <Text className="text-[20px] font-bold mb-1" style={{ color: "#22223b" }}>
+              {item.title}
+            </Text>
+            <Text className="text-[15px] mb-4" style={{ color: "#6c6c80" }}>
+              {item.description}
+            </Text>
+            <View className="flex-row justify-between items-center">
+              <View className="flex-row items-center text-[#ccc] rounded-l py-2 px-1">
+                <MaterialCommunityIcons name="fire" size={18} color="#ff9800" />
+                <Text className="ml-2 text-[#ff9800] font-bold text-[14px]">
+                  {item.streak_count} day streak
+                </Text>
+              </View>
+              <View className="bg-[#ede7f6] rounded-l py-2 px-1">
+                <Text className="font-bold text-[14px]" style={{ color: "#7c4dff" }}>
+                  {item.frequency}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Surface>
+    </Swipeable>
+  );
+
+  const ListEmptyComponent = () => (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-[#666666]">No Habits yet. Add your first Habit!</Text>
+    </View>
+  );
+
   return (
     <View className="flex-1">
-      <ScrollView className="flex-1 p-4" showsHorizontalScrollIndicator={false}>
-        {habitList.length ? (
-          habitList.map((item) => (
-            <Swipeable
-              containerStyle={{ marginBottom: 18 }}
-              key={`${item.id}}`}
-              ref={(ref) => {
-                swipeableRefs.current[item.id] = ref;
-              }}
-              overshootLeft={false}
-              overshootRight={false}
-              renderRightActions={() => renderRightActions(item)}
-              onSwipeableOpen={(direction) => {
-                // if (direction === "left") {
-                // }
-                // if (direction === "right") {
-                // }
-
-                closeOtherSwiper(item.id);
-              }}
-            >
-              <Surface
-                elevation={0}
-                style={{
-                  borderRadius: 18,
-                  backgroundColor: item.light,
-                  opacity: 0.8,
-                  shadowColor: "#000",
-                  shadowOffset: { width: 0, height: 2 },
-                  shadowOpacity: 0.08,
-                  shadowRadius: 8,
-                  elevation: 4,
-                }}
-              >
-                <TouchableWithoutFeedback onPress={() => closeOtherSwiper(item.id)}>
-                  <View className="p-5">
-                    <Text className="text-[20px] font-bold mb-1" style={{ color: "#22223b" }}>
-                      {item.title}
-                    </Text>
-                    <Text className="text-[15px] mb-4" style={{ color: "#6c6c80" }}>
-                      {item.description}
-                    </Text>
-                    <View className="flex-row justify-between items-center">
-                      <View className="flex-row items-center text-[#ccc] rounded-l py-2 px-1">
-                        <MaterialCommunityIcons name="fire" size={18} color="#ff9800" />
-                        <Text className="ml-2 text-[#ff9800] font-bold text-[14px]">
-                          {item.streak_count} day streak
-                        </Text>
-                      </View>
-                      <View className="bg-[#ede7f6] rounded-l py-2 px-1">
-                        <Text className="font-bold text-[14px]" style={{ color: "#7c4dff" }}>
-                          {item.frequency}
-                        </Text>
-                      </View>
-                    </View>
-                  </View>
-                </TouchableWithoutFeedback>
-              </Surface>
-            </Swipeable>
-          ))
-        ) : (
-          <View className="flex-1 justify-center items-center">
-            <Text className="text-[#666666]">No Habits yet. Add your first Habit!</Text>
-          </View>
-        )}
-      </ScrollView>
-      {/*<TouchableOpacity*/}
-      {/*  className="w-16 h-16 absolute right-6 bottom-[80px] rounded-full bg-[#2775f9] z-50 shadow-lg items-center justify-center"*/}
-      {/*  onPress={() => route.push("/(tabs)/add-habit")}*/}
-      {/*>*/}
-      {/*  <Ionicons name="pencil" color="#fff" size={24} />*/}
-      {/*</TouchableOpacity>*/}
+      <FlatList
+        data={habitList || []}
+        renderItem={({ item }) => renderItem(item)}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ padding: 6 }}
+        ListEmptyComponent={() => ListEmptyComponent()}
+        ListFooterComponent={() => <View style={{ paddingVertical: 40 }}></View>}
+      />
     </View>
   );
 };
